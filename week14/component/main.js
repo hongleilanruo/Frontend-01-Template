99c80bebbb62b73009740da757d79e2b3461916d
honglei <724021234@qq.com>
2020-07-27 22:50:49 +0800

    补作业

--- week14/component/main.js
+++ week14/component/main.js
@@ -1,70 +1,14 @@
-function createElement(Cls, attributes, ...children) {
-    let o;
-    if(typeof Cls === 'string') {
-        o = new Wrapper(Cls);
-    }else {
-        o = new Cls({
-            timer:{}
-        });
-    }
+import {createElement, Text, Wrapper} from './createElement';
 
-    
-
-    for (let name in attributes) {
-        o.setAttribute(name, attributes[name]);
-    }
-
-    // console.log(children);
-    for(let child of children) {
-        if(typeof child === 'string') {
-            child = new Text(child);
-        }
-        o.appendChild(child);
-    }
-    return o;
-}
-
-class Text {
-    constructor(text) {
-        this.children = [];
-        this.root = document.createTextNode(text);
-    }
-
-    mountTo(parent) {
-        parent.appendChild(this.root);
-    }
-}
-
-class Wrapper {
-    constructor(type) {
-        this.children = [];
-        this.root = document.createElement(type);
-    }
-
-    setAttribute(name, value) {//attribute
-        this.root.setAttribute(name, value);
-    }
-
-    appendChild(child) {
-        // child.mountTo(this.root);
-        this.children.push(child);
-    }
-
-    mountTo(parent) {
-        parent.appendChild(this.root);
-        for(let child of this.children) {
-            child.mountTo(this.root);
-        }
-    }
-}
-
-class MyComponent {
+class Carousel {
     constructor(config) {
         this.children = [];
+        this.attributes = new Map();
+        this.properties = new Map();
     }
 
     setAttribute(name, value) {//attribute
-        this.root.setAttribute(name, value);
+        this[name] = value;
     }
 
     appendChild(child) {
@@ -72,18 +16,43 @@ class MyComponent {
     }
 
     render() {
-        return <article>
-            <header>I'm a header</header>
-            {this.slot}
-            <footer>I'm a footer</footer>
-        </article>
+        let children = this.data.map(url => {
+            let element = <img src = {url}/>;
+            element.addEventListener('dragstart', event => event.preventDefault());
+            return element
+        });
+        let root = <div class="carousel">{children}</div>
+
+        let position = 0;
+        let nextPic = () => {
+            let nextPosition = (position + 1) % this.data.length;
+            let current = children[position];
+            let next = children[nextPosition];
+
+            current.style.transition = 'ease 0s';
+            next.style.transition = 'ease 0s';
+
+            current.style.transform = `translateX(${-100 * position}%)`;
+            next.style.transform = `translateX(${100 - 100 * nextPosition}%)`;
+
+            setTimeout(function () {
+                current.style.transition = 'ease 0.5s';
+                next.style.transition = 'ease 0.5s';
+
+                current.style.transform = `translateX(${-100 - 100 * position}%)`;
+                next.style.transform = `translateX(${-100 * nextPosition}%)`;
+
+                position = nextPosition;
+            }, 16);
+
+            setTimeout(nextPic, 3000);
+        }
+        setTimeout(nextPic, 3000);
+
+        return root;
     }
 
     mountTo(parent) {
-        this.slot = <div></div>
-        for(let child of this.children) {
-            this.slot.appendChild(child);
-        }
         this.render().mountTo(parent);
     }
 }
@@ -95,9 +64,13 @@ let component = <div id = "a" class = "b" style="width:100px;height:100px;backgr
         <div></div>
     </div>
 */
-let component = <MyComponent>
-    <div>text text text</div>
-</MyComponent>
+let component = <Carousel data = {[
+    "https://static001.geekbang.org/resource/image/bb/21/bb38fb7c1073eaee1755f81131f11d21.jpg",
+    "https://static001.geekbang.org/resource/image/1b/21/1b809d9a2bdf3ecc481322d7c9223c21.jpg",
+    "https://static001.geekbang.org/resource/image/b6/4f/b6d65b2f12646a9fd6b8cb2b020d754f.jpg",
+    "https://static001.geekbang.org/resource/image/73/e4/730ea9c393def7975deceb48b3eb6fe4.jpg",
+]} />
+
 
 component.mountTo(document.body);
 console.log(component);
