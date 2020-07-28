1ebe21eea1ba298d013aed5ec5570a0555e8fe59
honglei <724021234@qq.com>
2020-07-28 22:51:59 +0800

    补作业

--- week15/component/parser.js
+++ week15/component/parser.js
@@ -1,115 +1,13 @@
-const css = require('css');
-const EOF = Symbol("EOF");
-
 let currentToken = null;
 let currentAttribute = null;
+
+let stack = [{type: "document", children:[]}];
 let currentTextNode = null;
-let stack = [{
-    type: "document",
-    children: []
-}];
-let rules = [];
 
-function addCSSRules(text) {
-    let ast = css.parse(text);
-    rules.push(...ast.stylesheet.rules);
-}
-
-function match(element, selector) {
-    if (!selector || !element.attributes) {
-        return false;
-    }
-
-    if (selector.charAt(0) == '#') {
-        let attr = element.attributes.filter(attr => attr.name === 'id')[0];
-        if (attr && attr.value === selector.replace('#', '')) {
-            return true;
-        }
-    } else if (selector.charAt(0) == '.') {
-        let attr = element.attributes.filter(attr => attr.name === 'class')[0];
-        if (attr && attr.value === selector.replace('.', '')) {
-            return true;
-        }
-    } else {
-        if (element.tagName === selector) {
-            return true;
-        }
-    }
-}
-
-function specificity(selector) {
-    let p = [0, 0, 0, 0];
-    let selectorParts = selector.split(' ');
-    for (let part of selectorParts) {
-        if (part.charAt(0) == '#') {
-            p[1] += 1;
-        } else if (part.charAt(0) == '.') {
-            p[2] += 1;
-        } else {
-            p[3] += 1;
-        }
-    }
-    return p;
-}
-
-function compare(sp1, sp2) {
-    if (sp1[0] - sp2[2]) {
-        return sp1[0] - sp2[0];
-    }
-    if (sp1[1] - sp2[1]) {
-        return sp1[1] - sp2[1];
-    }
-    if (sp1[2] - sp2[2]) {
-        return sp1[2] - sp2[2];
-    }
-    return sp1[3] - sp2[3];
-}
-
-function computeCSS(element) {
-    let elements = stack.slice().reverse();
-    if (!element.computedStyle) {
-        element.computedStyle = {};
-    }
-
-    for (let rule of rules) {
-        let selectorParts = rule.selectors[0].split(' ').reverse();
-        if (!match(element, selectorParts[0])) {
-            continue;
-        }
-
-        let j = 1;
-
-        for (let i = 0; i < elements.length; i++) {
-            if (match(elements[i], selectorParts[j])) {
-                j++;
-            }
-        }
-        if (j >= selectorParts.length) {
-            matched = true;
-        }
-        if (matched) {
-            let sp1 = specificity(rule.selectors[0]);
-            let computedStyle = element.computedStyle;
-            for (let declaration of rule.declaration) {
-                if (!computedStyle[declaration.property]) {
-                    computedStyle[declaration.property] = {}
-                }
-                if (!computedStyle[declaration.property].specificity) {
-                    computedStyle[declaration.property].value = declaration.value;
-                    computedStyle[declaration.property].specificity = sp
-                } else if (compare(computedStyle[declaration.property].specificity, sp) < 0) {
-                    computedStyle[declaration.property].value = declaration.value
-                    computedStyle[declaration.property].specificity = sp
-                }
-            }
-        }
-    }
-}
-
-function emit(token) {
+function emit(token){
     let top = stack[stack.length - 1];
 
-    if (token.type == "startTag") {
+    if(token.type == "startTag") {
         let element = {
             type: "element",
             children: [],
@@ -118,42 +16,33 @@ function emit(token) {
 
         element.tagName = token.tagName;
 
-        for (let p in token) {
-            if (p != "type" && p != "tagName") {
+        for(let p in token) {
+            if(p != "type" || p != "tagName")
                 element.attributes.push({
                     name: p,
                     value: token[p]
                 });
-            }
         }
-
-        computeCSS(element);
 
         top.children.push(element);
 
-        if (!token.isSelfClosing) {
+        if(!token.isSelfClosing)
             stack.push(element);
-        }
 
         currentTextNode = null;
-
-    } else if (token.type == "endTage") {
-        if (top.tagName != token.tagName) {
+        
+    } else if(token.type == "endTag") {
+        if(top.tagName != token.tagName) {
             throw new Error("Tag start end doesn't match!");
         } else {
-            if (top.tagName === 'style') {
-                addCSSRules(top.children[0].content);
-            }
             stack.pop();
         }
-
         currentTextNode = null;
-
-    } else if (token.type == "text") {
-        if (currentTextNode == null) {
+    }  else if(token.type == "text") {
+        if(currentTextNode == null) {
             currentTextNode = {
                 type: "text",
-                content: ''
+                content: ""
             }
             top.children.push(currentTextNode);
         }
@@ -161,50 +50,53 @@ function emit(token) {
     }
 }
 
-function data(c) {
-    if (c == "<") {
+const EOF = Symbol("EOF");
+
+
+function data(c){
+    if(c == "<") {
         return tagOpen;
-    } else if (c === EOF) {
+    } else if( c == EOF) {
         emit({
-            type: "EOF"
+            type:"EOF"
         });
-        return;
+        return ;
     } else {
         emit({
-            type: "text",
-            content: c
-        })
+            type:"text",
+            content:c
+        });
         return data;
     }
 }
 
-function tagOpen(c) {
-    if (c == "/") {
+function tagOpen(c){
+    if(c == "/") {
         return endTagOpen;
-    } else if (c.match(/^[a-zA-Z]$/)) {
+    } else if(c.match(/^[a-zA-Z]$/)) {
         currentToken = {
             type: "startTag",
-            tagName: ""
+            tagName : ""
         }
         return tagName(c);
     } else {
         emit({
             type: "text",
-            content: c
-        })
-        return;
+            content : c
+        });
+        return ;
     }
 }
 
 function tagName(c) {
-    if (c.match(/^[\t\n\f ]$/)) {
+    if(c.match(/^[\t\n\f ]$/)) {
         return beforeAttributeName;
-    } else if (c == "/") {
+    } else if(c == "/") {
         return selfClosingStartTag;
-    } else if (c.match(/^[A-Z]$/)) {
-        currentToken.tagName += c;
+    } else if(c.match(/^[A-Z]$/)) {
+        currentToken.tagName += c//.toLowerCase();
         return tagName;
-    } else if (c == ">") {
+    } else if(c == ">") {
         emit(currentToken);
         return data;
     } else {
@@ -212,175 +104,338 @@ function tagName(c) {
         return tagName;
     }
 }
-
 function beforeAttributeName(c) {
-    if (c.match(/^[\t\n\f ]$/)) {
+    if(c.match(/^[\t\n\f ]$/)) {
         return beforeAttributeName;
-    } else if (c == "/" || c == ">" || c == EOF) {
+    } else if(c == "/" || c == ">" || c == EOF) {
         return afterAttributeName(c);
-    } else if (c == "=") {
+    } else if(c == "=") {
 
     } else {
         currentAttribute = {
             name: "",
             value: ""
         }
+        //console.log("currentAttribute", currentAttribute)
         return attributeName(c);
     }
 }
 
 function attributeName(c) {
-    if (c.match(/^[\t\n\f ]$/) || c == "/" || c == ">" || c == EOF) {
+    //console.log(currentAttribute);
+    if(c.match(/^[\t\n\f ]$/) || c == "/" || c == ">" || c == EOF) {
         return afterAttributeName(c);
-    } else if (c == "=") {
+    } else if(c == "=") {
         return beforeAttributeValue;
-    } else if (c == "\u0000") {
-
-    } else if (c == "\"" || c == "'" || c == "<") {
-
+    } else if(c == "\u0000") {
+    
+    } else if(c == "\"" || c == "'" || c == "<") {
+    
     } else {
-        currentAttribute += c;
+        currentAttribute.name += c;
         return attributeName;
     }
 }
 
-function beforeAttributeValue(c) {
-    if (c.match(/^[\t\n\f ]$/) || c == "/" || c == ">" || c == EOF) {
-        return beforeAttributeValue;
-    } else if (c == "\"") {
-        return doubleQuotedAttributeValue;
-    } else if (c == "\'") {
-        return singleQuotedAttributeValue;
-    } else if (c == ">") {
 
+function beforeAttributeValue(c) {
+    if(c.match(/^[\t\n\f ]$/) || c == "/" || c == ">" || c == EOF) {
+        return beforeAttributeValue;
+    } else if(c == "\"") {
+        return doubleQuotedAttributeValue;
+    } else if(c == "\'") {
+        return singleQuotedAttributeValue;
+    } else if(c == ">") {
+        //return data;
     } else {
         return UnquotedAttributeValue(c);
     }
 }
 
 function doubleQuotedAttributeValue(c) {
-    if (c == "\"") {
+    if(c == "\"") {
         currentToken[currentAttribute.name] = currentAttribute.value;
         return afterQuotedAttributeValue;
-    } else if (c == "\u0000") {
+    } else if(c == "\u0000") {
 
-    } else if (c == EOF) {
-
+    } else if(c == EOF) {
+        
     } else {
         currentAttribute.value += c;
-        return doubleQuotedAttributeValue;
+        return doubleQuotedAttributeValue
     }
 }
+
 
 function singleQuotedAttributeValue(c) {
-    if (c == "\'") {
+    if(c == "\'") {
         currentToken[currentAttribute.name] = currentAttribute.value;
         return afterQuotedAttributeValue;
-    } else if (c == "\u0000") {
+    } else if(c == "\u0000") {
 
-    } else if (c == EOF) {
-
+    } else if(c == EOF) {
+        
     } else {
         currentAttribute.value += c;
-        return doubleQuotedAttributeValue;
+        return doubleQuotedAttributeValue
     }
 }
 
-function afterQuotedAttributeValue(c) {
-    if (c.match(/^[\t\n\f ]$/)) {
+function afterQuotedAttributeValue (c){
+    if(c.match(/^[\t\n\f ]$/)) {
         return beforeAttributeName;
-    } else if (c == "/") {
+    } else if(c == "/") {
         return selfClosingStartTag;
-    } else if (c == ">") {
+    } else if(c == ">") {
         currentToken[currentAttribute.name] = currentAttribute.value;
         emit(currentToken);
         return data;
-    } else if (c == EOF) {
-
+    } else if(c == EOF) {
+        
     } else {
         currentAttribute.value += c;
-        return doubleQuotedAttributeValue;
+        return doubleQuotedAttributeValue
     }
 }
+
 
 function UnquotedAttributeValue(c) {
-    if (c.match(/^[\t\n\f ]$/)) {
+
+    if(c.match(/^[\t\n\f ]$/)) {
         currentToken[currentAttribute.name] = currentAttribute.value;
         return beforeAttributeName;
-    } else if (c == "/") {
+    } else if(c == "/") {
         currentToken[currentAttribute.name] = currentAttribute.value;
         return selfClosingStartTag;
-    } else if (c == ">") {
+    } else if(c == ">") {
         currentToken[currentAttribute.name] = currentAttribute.value;
         emit(currentToken);
         return data;
-    } else if (c == "\u0000") {
+    } else if(c == "\u0000") {
 
-    } else if (c == "\"" || c == "'" || c == "<" || c == "=" || c == "`") {
+    } else if(c == "\"" || c == "'" || c == "<" || c == "=" || c == "`") {
 
-    } else if (c == EOF) {
-
+    } else if(c == EOF) {
+        
     } else {
         currentAttribute.value += c;
-        return UnquotedAttributeValue;
+        return UnquotedAttributeValue
     }
 }
 
-function selfClosingStartTag(c) {
-    if (c == ">") {
+function selfClosingStartTag(c){
+    if( c == ">") {
         currentToken.isSelfClosing = true;
         emit(currentToken);
         return data;
-    } else if (c == "EOF") {
+    } else if(c == "EOF") {
 
+    } else {
+        
+    }
+}
+
+function endTagOpen(c){
+    if(c.match(/^[a-zA-Z]$/)) {
+        currentToken = {
+            type: "endTag",
+            tagName : ""
+        }
+        return tagName(c);
+    } else if(c == ">") {
+
+    } else if(c == EOF) {
+        
     } else {
 
     }
 }
-
-function endTagOpen(c) {
-    if (c.match(/^[a-zA-Z]$/)) {
-        currentToken = {
-            type: "endTag",
-            tagName: ""
-        }
-        return tagName(c);
-    } else if (c == ">") {
-
-    } else if (c == EOF) {
-
+//in script
+function scriptData(c){
+    
+    if(c == "<") {
+        return scriptDataLessThanSign;
     } else {
+        emit({
+            type:"text",
+            content:c
+        });
+        return scriptData;
+    }
+}
+//in script received <
+function scriptDataLessThanSign(c){
+    if(c == "/") {
+        return scriptDataEndTagOpen;
+    } else {
+        emit({
+            type:"text",
+            content:"<"
+        });
+        emit({
+            type:"text",
+            content:c
+        });
+        return scriptData;
+    }
+}
+//in script received </
+function scriptDataEndTagOpen(c){
+    if(c == "s") {
+        return scriptDataEndTagNameS;
+    } else {
+        emit({
+            type:"text",
+            content:"<"
+        });
 
+        emit({
+            type:"text",
+            content:"/"
+        });
+
+        emit({
+            type:"text",
+            content:"c"
+        });
+        return scriptData;
+    }
+}
+//in script received </s
+function scriptDataEndTagNameS(c){
+    if(c == "c") {
+        return scriptDataEndTagNameC;
+    } else {
+        emit({
+            type:"text",
+            content:"</s"
+        });
+        emit({
+            type:"text",
+            content:c
+        });
+        return scriptData;
+    }
+}
+
+//in script received </sc
+function scriptDataEndTagNameC(c){
+    if(c == "r") {
+        return scriptDataEndTagNameR;
+    } else {
+        emit({
+            type:"text",
+            content:"</sc"
+        });
+        emit({
+            type:"text",
+            content:c
+        });
+        return scriptData;
+    }
+}
+
+//in script received </scr
+function scriptDataEndTagNameR(c){
+    if(c == "i") {
+        return scriptDataEndTagNameI;
+    } else {
+        emit({
+            type:"text",
+            content:"</scr"
+        });
+        emit({
+            type:"text",
+            content:c
+        });
+        return scriptData;
+    }
+}
+//in script received </scri
+function scriptDataEndTagNameI(c){
+    if(c == "p") {
+        return scriptDataEndTagNameP;
+    } else {
+        emit({
+            type:"text",
+            content:"</scri"
+        });
+        emit({
+            type:"text",
+            content:c
+        });
+        return scriptData;
+    }
+}
+//in script received </scrip
+function scriptDataEndTagNameP(c){
+    if(c == "t") {
+        return scriptDataEndTag;
+    } else {
+        emit({
+            type:"text",
+            content:"</scrip"
+        });
+        emit({
+            type:"text",
+            content:c
+        });
+        return scriptData;
+    }
+}
+//in script received </script
+function scriptDataEndTag(c){
+    if(c == " ") {
+        return scriptDataEndTag;
+    } if(c == ">") {
+        emit({
+            type: "endTag",
+            tagName : "script"
+        });
+        return data;
+    } else {
+        emit({
+            type:"text",
+            content:"</script"
+        });
+        emit({
+            type:"text",
+            content:c
+        });
+        return scriptData;
     }
 }
 
 function afterAttributeName(c) {
-    if (c.match(/^[\t\n\f ]$/)) {
+    if(c.match(/^[\t\n\f ]$/)) {
         return afterAttributeName;
-    } else if (c == "/") {
+    } else if(c == "/") {
         return selfClosingStartTag;
-    } else if (c == "=") {
+    } else if(c == "=") {
         return beforeAttributeValue;
-    } else if (c == ">") {
+    } else if(c == ">") {
         currentToken[currentAttribute.name] = currentAttribute.value;
         emit(currentToken);
         return data;
-    } else if (c == EOF) {
-
+    } else if(c == EOF) {
+        
     } else {
         currentToken[currentAttribute.name] = currentAttribute.value;
         currentAttribute = {
-            name: "",
-            value: ""
-        }
+            name : "",
+            value : ""
+        };
         return attributeName(c);
     }
 }
 
-module.exports.parseHTML = function parseHTML(html) {
+module.exports.parseHTML = function parseHTML(html){
     let state = data;
-    for (let c of html) {
+    for(let c of html) {
         state = state(c);
+        if(stack[stack.length - 1].tagName === "script" && state == data) {
+            state = scriptData;
+        }
     }
     state = state(EOF);
     return stack[0];
