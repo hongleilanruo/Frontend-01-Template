694202739a96dc4a4b4a279cfb5ca15e00535f52
honglei <724021234@qq.com>
2020-07-31 20:11:37 +0800

    补作业

--- week15/animation.js
+++ week15/animation.js
@@ -1,28 +1,49 @@
 export class Timeline {
     constructor() {
         this.animation = []
-    }
-    tick() {
-        let t = Date.now() - this.startTime
-        for (let animation of this.animation) {
-            if (t > animation.duration + animation.delay) continue
+        this.reuestID = null
+        this.tick = () => {
+            let t = Date.now() - this.startTime
+            let animation = this.animations.filter(animation => !animation.finished)
+            for (let animation of this.animation) {
+                let {
+                    object,
+                    property,
+                    template,
+                    start,
+                    end,
+                    duration,
+                    timingFunction
+                } = animation
 
-            let {
-                object,
-                property,
-                template,
-                start,
-                end,
-                duration,
-                timingFunction
-            } = animation
+                let progression = timingFunction((t - delay) / duration)
 
-            let progression = timingFunction((t - delay) / duration)
-            let value = start + progression * (end - start)
+                if (t > animation.duration + animation.delay) {
+                    progression = 1
+                    animation.finished = true
+                }
 
-            object[property] = template(value)
+                let value = start + progression * (end - start)
+
+                object[property] = template(value)
+            }
+
+            if (animations.length) {
+                this.reuestID = requestAnimationFrame(this.thick)
+            }
         }
-        requestAnimationFrame(() => this.thick())
+    }
+
+    pause() {
+        this.pauseTime = Date.now()
+        if (this.reuestID !== null) {
+            cancelAnimationFrame(this.reuestID)
+        }
+    }
+
+    resume() {
+        this.startTime += Date.now() - this.pauseTime
+        this.tick()
     }
 
     start() {
