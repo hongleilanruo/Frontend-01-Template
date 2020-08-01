3967473ca35010fa7ded23153e57d6682601ca9a
honglei <724021234@qq.com>
2020-08-01 18:15:09 +0800

    补作业

--- week15/animation.js
+++ week15/animation.js
@@ -2,6 +2,7 @@ export class Timeline {
     constructor() {
         this.animation = []
         this.reuestID = null
+        this.state = 'inited'
         this.tick = () => {
             let t = Date.now() - this.startTime
             let animation = this.animations.filter(animation => !animation.finished)
@@ -16,14 +17,14 @@ export class Timeline {
                     timingFunction
                 } = animation
 
-                let progression = timingFunction((t - delay) / duration)
+                let progression = timingFunction((t - delay - startTime) / duration)
 
-                if (t > animation.duration + animation.delay) {
+                if (t > duration + delay + startTime) {
                     progression = 1
                     animation.finished = true
                 }
 
-                let value = start + progression * (end - start)
+                let value = animation.valueFromProgression(progression)
 
                 object[property] = template(value)
             }
@@ -35,6 +36,8 @@ export class Timeline {
     }
 
     pause() {
+        if (this.state !== 'playing') return
+        this.state = 'paused'
         this.pauseTime = Date.now()
         if (this.reuestID !== null) {
             cancelAnimationFrame(this.reuestID)
@@ -42,29 +45,75 @@ export class Timeline {
     }
 
     resume() {
+        if (this.state !== 'paused') return
+        this.state = 'playing'
         this.startTime += Date.now() - this.pauseTime
         this.tick()
     }
 
     start() {
+        if (this.state !== 'inited') return
+        this.state = 'playing'
         this.startTime = Date.now()
         this.tick()
     }
 
-    add(animation) {
+    restart() {
+        if (this.state === 'playing') {
+            this.pause()
+        }
+        this.animation = []
+        this.reuestID = null
+        this.state = 'playing'
+        this.startTime = Date.now()
+        this.pauseTime = null
+        this.tick();
+    }
+
+    add(animation, startTime) {
         this.animation.push(animation)
+        animation.finished = false
+        if (this.state === 'playing') {
+            animation.startTime = startTime !== void 0 ? startTime : Date.now() - this.startTime
+        } else {
+            animation.startTime = startTime !== void 0 ? startTime : 0
+        }
     }
 }
 
 export class Animation {
-    constructor(object, property, template, start, end, duration, delay, timingFunction) {
+    constructor(object, property, start, end, duration, delay, timingFunction, template) {
         this.object = object
         this.property = property
-        this.template = template
+        this.template = template || (v => `rgba(${v.r},${v.g},${v.b},${v.a})`)
         this.start = start
         this.end = end
         this.duration = duration
         this.delay = delay
         this.timingFunction = timingFunction
+    }
+    valueFromProgression(progression) {
+        return this.start + progression * (this.end - this.start)
+    }
+}
+
+export class ColorAnimation {
+    constructor(object, property, start, end, duration, delay, timingFunction, template) {
+        this.object = object
+        this.property = property
+        this.template = template || (v => `rgba(${v.r},${v.g},${v.b},${v.a})`)
+        this.start = start
+        this.end = end
+        this.duration = duration
+        this.delay = delay
+        this.timingFunction = timingFunction
+    }
+    valueFromProgression(progression) {
+        return {
+            r: this.start.r + progression * (this.end.r - this.start.r),
+            g: this.start.g + progression * (this.end.g - this.start.g),
+            b: this.start.b + progression * (this.end.b - this.start.b),
+            a: this.start.a + progression * (this.end.a - this.start.a)
+        }
     }
 }
